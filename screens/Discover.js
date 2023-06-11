ba1de39260781bf70eaaec70a8fe32bd3a598bc0
Nestor <nesty9306@gmail.com>
2023-06-11 04:03:55 -0500

    firts

---
+++ screens/Discover.js
@@ -0,0 +1,15 @@
+import { Text, View, Image } from 'react-native'
+import React, { Component } from 'react'
+import Avatar from "../assets/avatar.png"
+export class Discover extends Component {
+  render() {
+    return (
+      <View   className="flex-1 bg-white">
+        <Text>1asdasdasdasd</Text>
+        
+      </View>
+    )
+  }
+}
+
+export default Discover
